bc5dbe85e51ea52ec7aa440901be9f30cc6fbb9a
Zak Burki <zakir.burki@gmail.com>
2014-02-13 19:31:06 +0800

    starter framework for final.

--- lesson6/RPS/RPS.js
+++ lesson6/RPS/RPS.js
@@ -1,3 +1,16 @@
+     var userGuess = prompt("Rock/Paper/Scissors?");
+
+            document.write("You have selected: " + userGuess + "<br>");
+
+            setTimeout(function(){
+
+                var computerGuess = convert(Math.floor(Math.random()*3));
+
+                document.write("The computer chooses: " + computerGuess + "<br>");
+
+                document.write(compare(userGuess.toLowerCase(),computerGuess.toLowerCase
+                },1000);
+
 function convert(number) {
     if (number === 0) {
         return "Rock";
@@ -10,24 +23,24 @@ function convert(number) {
 
 function compare(userGuess, computerGuess) {
     if (userGuess === computerGuess) {
-        return "Boom - The result is a draw!"
+        return "Boom - The result is a draw!";
     } else if (userGuess === "rock") {
         if (computerGuess === "scissors") {
-            return "You win with Rock!"
+            return "You win with Rock!";
         } else {
-            return "Sorry - computer wins with Paper"
+            return "Sorry - computer wins with Paper";
         }
     } else if (userGuess === "scissors") {
         if (computerGuess === "paper") {
-            return "You win with Scissors!"
+            return "You win with Scissors!";
         } else {
-            return "Sorry - computer wins with Rock"
+            return "Sorry - computer wins with Rock";
         }
     } else if (userGuess === "paper") {
         if (computerGuess === "rock") {
-            return "You win with Paper"
+            return "You win with Paper";
         } else {
-            return "Sorry - computer wins with Scissors"
+            return "Sorry - computer wins with Scissors";
         }
     }
 }
---
+++ lesson6/RPS/random.js
@@ -0,0 +1,203 @@
+#!/usr/bin/env node
+http  = require("http");
+https = require("https");
+const VERSION = "1.0.0beta-1"
+function globalErrorCallback(type,code,string){
+    console.error("RANDOM.ORG Error: Type: "+type+", Status Code: "+code+", Response Data: "+string);
+};
+function formatStrings(strings){
+    return strings.toString().trim().split("\n");
+}
+function formatNumbers(numbers){
+    var rows = numbers.toString().trim().split("\n");
+    for(var i=0;i<rows.length;i++){
+        rows[i]=rows[i].split("\t");
+    }
+    return rows;
+}
+function getOptions(options,defaults){
+	if(!options){
+        options = [];
+	}
+	for(var i in defaults){
+		if(!options[i]){
+			options[i] = defaults[i];
+		}
+	}
+	return options;
+};
+exports.generateIntegers = function(callback,options,errorCallback){
+	var defaults = {
+		secure: false,
+		num: 1,
+		min: 0,
+		max: 10000,
+		col: 1,
+		base: 10,
+		rnd: "new"
+	};
+	if(!callback){
+	   callback = console.log;
+	}
+	if(!errorCallback){
+	   errorCallback = globalErrorCallback;
+	}
+	var scheme;
+	var opts = getOptions(options,defaults);
+	if(opts.secure){
+		scheme = https;
+	}else{
+		scheme = http;
+	}
+	var callbackFunction = function(res){
+		res.on("data",function(data){
+			if(res.statusCode == 200){
+				callback(formatNumbers(data));
+			}else if(res.statusCode == 503){
+				errorCallback("ServerError",503,data);
+			}else{
+				errorCallback("RequestError",res.statusCode,data);
+			}
+		});
+	}
+	var req = scheme.request({
+		host: "www.random.org",
+		port: opts.secure?443:80,
+		method: "GET",
+		path: "/integers/?format=plain&num="+opts.num+"&min="+opts.min+"&max="+opts.max+"&col="+opts.col+"&base="+opts.base+"&rnd="+opts.rnd,
+		headers: {
+			"User-Agent": "Random.org client for Node.js by Rodger Combs <rodger.combs@gmail.com>, version "+VERSION
+		}
+	},callbackFunction);
+	req.end();
+};
+exports.generateSequence = function(callback,options,errorCallback){
+	var defaults = {
+		secure: false,
+		min: 0,
+		max: 10,
+		col: 1,
+		base: 10,
+		rnd: "new"
+	};
+	if(!callback){
+	   callback = console.log;
+	}
+	if(!errorCallback){
+	   errorCallback = globalErrorCallback;
+	}
+	var scheme;
+	var opts = getOptions(options,defaults);
+	if(opts.secure){
+		scheme = https;
+	}else{
+		scheme = http;
+	}
+	var callbackFunction = function(res){
+		res.on("data",function(data){
+			if(res.statusCode == 200){
+				callback(formatNumbers(data));
+			}else if(res.statusCode == 503){
+				errorCallback("ServerError",503,data);
+			}else{
+				errorCallback("RequestError",res.statusCode,data);
+			}
+		});
+	}
+	var req = scheme.request({
+		host: "www.random.org",
+		port: opts.secure?443:80,
+		method: "GET",
+		path: "/sequences/?format=plain&min="+opts.min+"&max="+opts.max+"&col="+opts.col+"&base="+opts.base+"&rnd="+opts.rnd,
+		headers: {
+			"User-Agent": "Random.org client for Node.js by Rodger Combs <rodger.combs@gmail.com>, version "+VERSION
+		}
+	},callbackFunction);
+	req.end();
+};
+exports.generateStrings = function(callback,options,errorCallback){
+	var defaults = {
+		secure: false,
+        num: 1,
+        len: 10,
+        digits: true,
+		upper: true,
+		lower: true,
+		unique: false,
+		rnd: "new"
+	};
+	if(!callback){
+	   callback = console.log;
+	}
+	if(!errorCallback){
+	   errorCallback = globalErrorCallback;
+	}
+	var scheme;
+	var opts = getOptions(options,defaults);
+	if(opts.secure){
+		scheme = https;
+	}else{
+		scheme = http;
+	}
+	var callbackFunction = function(res){
+		res.on("data",function(data){
+			if(res.statusCode == 200){
+				callback(formatStrings(data));
+			}else if(res.statusCode == 503){
+				errorCallback("ServerError",503,data);
+			}else{
+				errorCallback("RequestError",res.statusCode,data);
+			}
+		});
+	}
+	var req = scheme.request({
+		host: "www.random.org",
+		port: opts.secure?443:80,
+		method: "GET",
+		path: "/strings/?format=plain&num="+opts.num+"&len="+opts.len+"&digits="+(opts.digits?"on":"off")+"&upperalpha="+(opts.upper?"on":"off")+"&loweralpha="+(opts.lower?"on":"off")+"&unique="+(opts.unique?"on":"off")+"&rnd="+opts.rnd,
+		headers: {
+			"User-Agent": "Random.org client for Node.js by Rodger Combs <rodger.combs@gmail.com>, version "+VERSION
+		}
+	},callbackFunction);
+	req.end();
+};
+exports.checkQuota = function(callback,options,errorCallback){
+	var defaults = {
+		secure: false,
+        ip: false
+	};
+	if(!callback){
+	   callback = console.log;
+	}
+	if(!errorCallback){
+	   errorCallback = globalErrorCallback;
+	}
+	var scheme;
+	var opts = getOptions(options,defaults);
+	if(opts.secure){
+		scheme = https;
+	}else{
+		scheme = http;
+	}
+	var callbackFunction = function(res){
+		res.on("data",function(data){
+			if(res.statusCode == 200){
+				callback(parseInt(data));
+			}else if(res.statusCode == 503){
+				errorCallback("ServerError",503,data);
+			}else{
+				errorCallback("RequestError",res.statusCode,data);
+			}
+		});
+	}
+	var req = scheme.request({
+		host: "www.random.org",
+		port: opts.secure?443:80,
+		method: "GET",
+		path: "/quota/?format=plain"+(opts.ip?"&ip="+opts.ip:""),
+		headers: {
+			"User-Agent": "Random.org client for Node.js by Rodger Combs <rodger.combs@gmail.com>, version "+VERSION
+		}
+	},callbackFunction);
+	req.end();
+};
